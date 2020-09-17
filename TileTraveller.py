a0056f33747a335439e60819ab797fdfd99e66ae
Sindri Thor <sindrihar@hotmail.com>
2020-09-17 14:05:45 +0000

    First itteration

---
+++ TileTraveller.py
@@ -0,0 +1,52 @@
+# https://github.com/SindriTh/TileTraveller
+
+str11 = "n"
+str12 = "nes"
+str13 = "es"
+str21 = "n"
+str22 = "sw"
+str23 = "we"
+str31 = "n"
+str32 = "ns"
+str33 = "sw"
+stadsetning = 11
+
+## Function 1 printallowed
+
+
+## Function 2 getinput
+
+
+## Function 3 allowed
+
+
+## Funtion 4 nextTile
+
+
+if(stadsetning == 11):
+    printallowed(str11)
+    getinput(str11)
+elif(stadsetning == 12):
+    printallowed(str12)
+    getinput(str12)
+elif(stadsetning == 13):
+    printallowed(str13)
+    getinput(str13)
+elif(stadsetning == 21):
+    printallowed(str21)
+    getinput(str21)
+elif(stadsetning == 22):
+    printallowed(str22)
+    getinput(str22)
+elif(stadsetning == 23):
+    printallowed(str23)
+    getinput(str23)
+elif(stadsetning == 31):
+    printallowed(str31)
+    getinput(str31)
+elif(stadsetning == 32):
+    printallowed(str32)
+    getinput(str32)
+elif(stadsetning == 33):
+    printallowed(str32)
+    getinput(str33)
